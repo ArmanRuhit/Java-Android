c76abc1774c457fba4a7da6b3a2150ef9ba61cb2
ArmanRuhit <armanruhit09@gmail.com>
2022-06-17 12:47:00 +0600

    Storage Permission
    Learned to add storage permission

---
+++ MLKitProject/app/src/main/java/com/example/mlkitproject/helper/ImageHelperActivity.java
@@ -0,0 +1,24 @@
+package com.example.mlkitproject.helper;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.example.mlkitproject.R;
+
+public class ImageHelperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_helper);
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+        }
+    }
+}
