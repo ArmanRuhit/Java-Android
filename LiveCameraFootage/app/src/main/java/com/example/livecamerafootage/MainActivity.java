6469458521ccded3dedc1d99f79bba13e121b9e0
ArmanRuhit <armanruhit09@gmail.com>
2022-07-02 01:19:40 +0600

    .

--- LiveCameraFootage/app/src/main/java/com/example/livecamerafootage/MainActivity.java
+++ LiveCameraFootage/app/src/main/java/com/example/livecamerafootage/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.livecamerafootage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO ask for camera permissions
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 121);
+        } else{
+            //TODO show live camera footage
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //TODO show live camera footage
+        if(grantResults.length > 0  && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            //TODO show
+        } else{
+            finish();
+        }
     }
 }
